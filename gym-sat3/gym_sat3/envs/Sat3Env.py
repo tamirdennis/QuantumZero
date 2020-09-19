9de73d5ef4a080e9883544144183311d8ebab414
tamir <tamir.d@futurelearning.ai>
2020-09-19 12:08:53 +0300

    initial commit

---
+++ gym-sat3/gym_sat3/envs/Sat3Env.py
@@ -0,0 +1,22 @@
+import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
+
+
+class Sat3Env(gym.Env):
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self):
+        ...
+
+    def step(self, action):
+        ...
+
+    def reset(self):
+        ...
+
+    def render(self, mode='human'):
+        ...
+
+    def close(self):
+        ...
